28165db2dd05961c85c7bb5400e5160c43d6b897
adeiza21 <adeiza220@gmail.com>
2023-05-19 14:15:40 -0700

    akh

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main (void) {
+	printf("Size of a char: %zu bytes",sizeof(char))
+	printf("Size of a char: %zu bytes",sizeof(int))
+	printf("Size of a char: %zu bytes",sizeof(long int))
+	printf("Size of a char: %zu bytes",sizeof(long long int))
+	printf("Size of a char: %zu bytes",sizeof(float))
+}
