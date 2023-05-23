594f7aa78fe3cf79569578eb4a78724ccb60c280
adeiza21 <adeiza220@gmail.com>
2023-05-23 11:35:17 -0700

    adfsdf

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ int main(void)
 	}
 	else if ((n % 10) < 6 && n != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not zero\n", n, n % 10)
+		printf("Last digit of %d is %d and is less than 6 and not zero\n", n, n % 10);
 	}
 	return (0);
 }
